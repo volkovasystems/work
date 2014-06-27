9623f49104fc4a6818274a0f3106caf92a8c4e97
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-06-27 08:14:19 +0800

    Simplify the work procedure. Much better and cleaner.

--- work.js
+++ work.js
@@ -1,69 +1,18 @@
-try{ var base = window; }catch( error ){ var base = exports; }
-( function module( base ){
-	define( "work",
-		[
-			"child_process",
-			"argumentsToArray",
-		],
-		function construct( ){
-			var work = function work( command, validator, callback ){
-				var parameters = argumentsToArray( arguments );
+/*:
+*/
+var work = function work( command, callback, validator ){
+	var task = childprocess.exec( command,
+		function onResult( error, output, errorOutput ){
+			if( error || errorOutput ){
+				error = error || new Error( errorOutput );
+			}
 
-				if( typeof command != "string" ){
-					throw new Error( "invalid command" );
-				}
-
-				if( parameters.length == 2 ){
-					callback = parameters[ 1 ];
-				}else if( parameters.length == 3 ){
-					validator = parameters[ 1 ];
-					callback = parameters[ 2 ];
-				}
-
-				if( validator
-					&& ( typeof validator != "function"
-					|| validator.name !== "validator" ) )
-				{
-					throw new Error( "invalid validator" );
-				}
-
-				if( callback
-					&& ( typeof callback != "function"
-					|| callback.name !== "callback" ) )
-				{
-					throw new Error( "invalid callback" );
-				}
-
-				var error = "";
-				var output = "";
-				
-				var task = childprocess.exec( command );
-				
-				task.stdout.on( "data",
-					function onData( data ){
-						output += data.toString( );
-					} );
-				
-				task.stderr.on( "data",
-					function onErrorData( data ){
-						error += data.toString( ).replace( /^\s+|\s+$/g, "" );
-					} );
-				
-				task.on( "close",
-					function onClose( ){
-						if( error ){
-							error = new Error( error );
-							callback( error );
-						}else{
-							var isValid = true;
-							if( validator ){
-								isValid = validator( output );
-							}
-							callback( null, isValid, output );
-						}
-					} );
-			};
-
-			base.work = work;
+			var isValid = !!output;
+			if( validator && output.trim( ) ){
+				isValid = validator( output );
+			}
+			
+			callback( error, isValid, output );
 		} );
-} )( base )
+};
+exports.work = work;
